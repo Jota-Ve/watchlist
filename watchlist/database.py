123e56ba3fc095169d65702e6ce3281dacf3eaab
João Vitor Barbosa <j.vs.b13@gmail.com>
2023-01-05 22:02:53 -0300

    Adiciona função para selecionar filme pelo ID

--- watchlist/database.py
+++ watchlist/database.py
@@ -1,1 +1,15 @@
-class MoviesTable: ...
+import sqlite3 as sql3
+
+
+class MoviesTable:
+    columns = ('movieID', 'primaryTitle', 'originalTitle', 'yearRelease', 'runtimeMinutes', 'genres')
+
+    def __init__(self, connection: str) -> None:
+        self.connection = sql3.Connection(connection)
+        self.connection.row_factory = sql3.Row
+
+    def get_movie_by_ID(self, ID: str) -> sql3.Row:
+        res = self.connection.execute("""SELECT * FROM Movies WHERE movieID=:id""",
+                                      { 'id': ID})
+
+        return res.fetchone()
--- watchlist/tests/test_database.py
+++ watchlist/tests/test_database.py
@@ -1,5 +1,13 @@
-import sys
-from pathlib import Path
+import pytest
 
-sys.path.append(str(Path(__file__).parent.parent))
-from database import MoviesTable
+from watchlist.database import MoviesTable
+
+
+class TestMoviesTable:
+    @pytest.fixture
+    def movies_table(self):
+        return MoviesTable('database\watchlist.db')
+
+    def test_get_movie_by_ID(self, movies_table: MoviesTable):
+        movie = movies_table.get_movie_by_ID('tt0133093')
+        assert movie['primaryTitle'] == 'The Matrix'
